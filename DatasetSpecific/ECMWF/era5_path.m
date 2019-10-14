43cd336510d68e65ded21fc98a4bc1b116512056
Corwin Wright <git.hub@corwin.me.uk>
2019-10-14 11:27:26 +0100

    test

--- DatasetSpecific/ECMWF/era5_path.m
+++ DatasetSpecific/ECMWF/era5_path.m
@@ -1,15 +1,21 @@
-function FilePath = era5_path(RootPath,DateNum)
+function FilePath = era5_path(DateNum,RootPath)
 
 %takes a Matlab datenumand generates a relative ERA5 path using my file
 %storage conventions
 %Corwin Wright, c.wright@bath.ac.uk, 14/OCT/2019
 
 
+if nargin == 1; RootPath = LocalDataDir; end
+
+%identify year and doy
 [y,~,~] = datevec(DateNum);
 dn = date2doy(floor(DateNum));
 
-FilePath = [RootPath,'/',sprintf('%04d',y),'/', ...
+%generate filepath
+FilePath = [RootPath,'/ERA5/',sprintf('%04d',y),'/', ...
                      '/','era5_',sprintf('%04d',y),'d',sprintf('%03d',dn),'.nc'];
-return                   
+                   
+                   
+return                  
 
 
