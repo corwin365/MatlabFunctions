8ec4ad71eb7ab0276d02e1c5819b7c078658a8bd
Corwin Wright <git.hub@corwin.me.uk>
2020-06-21 14:09:40 +0100

    converted h2p and p2h to run in n dimensions

--- GeophysicalAndTime/h2p.m
+++ GeophysicalAndTime/h2p.m
@@ -2,8 +2,18 @@
 %wrapper for alt2pres_complex
 
 function p = h2p(alt)
-  p = alt2pres_complex(alt);
 
+
+  %rearrange data into a line
+  sz = size(alt);
+  alt = alt(:);
+  
+  p = alt2pres_complex(alt);
+  
+  %and convert back
+  p = reshape(p,sz);
+    
+  
 end % end cycling through all of alt's element(s)...
 
 
--- GeophysicalAndTime/p2h.m
+++ GeophysicalAndTime/p2h.m
@@ -2,8 +2,19 @@
 %wrapper for pres2alt_complex
 function z = p2h(pres)
 
-   z = pres2alt_complex(pres);
 
+  %rearrange data into a line
+  sz = size(pres);
+  pres = pres(:);
+  
+  
+  %process
+  z = pres2alt_complex(pres);
+
+  
+  %and convert back
+  z = reshape(z,sz);
+  
 
 
 end 
