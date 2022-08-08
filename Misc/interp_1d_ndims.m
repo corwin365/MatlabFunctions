ff7dbbbc85c7c281d4cf3dfed3e70a78bcecf640
Corwin Wright <git.hub@corwin.me.uk>
2022-08-08 09:50:41 +0100

    added vargin passthrough for interpND

--- Misc/interp_1d_ndims.m
+++ Misc/interp_1d_ndims.m
@@ -1,14 +1,17 @@
-function yi= interp_1d_ndims(x,y,xi,Dim)
+function yi= interp_1d_ndims(x,y,xi,Dim,varargin)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Do 1d interpolation along a chosen dimension of an ND array
 %Corwin Wright, c.wright@bath.ac.uk, 2022/08/06
 %
-%inputs: as interp1, except with the dimension specified as the last input
-%outputs: array in original form except for the specified dimension
+%inputs: as interp1, except with the dimension specified as the last non-varargin input (set to [] to skip)
+%outputs: array interpolated along the chosen dimension
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%assume first dimension if not specified
+if nargin < 4; Dim = 1; end
+if numel(Dim) == 0; Dim = 1; end
 
 
 %get size of array
@@ -21,7 +24,7 @@ DimOrder = unique([Dim,1:1:numel(sz)],'stable');
 y = reshape(y,[sz(Dim),prod(sz(DimOrder(2:end)))]);
 
 %interpolate
-yi = interp1(x,y,xi);
+yi = interp1(x,y,xi,varargin{:});
 
 %reshape back
 yi = reshape(yi,[numel(xi),sz(DimOrder(2:end))]);
