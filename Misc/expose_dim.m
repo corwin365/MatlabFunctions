5e773b7ab3bbe440899bedf93b6bb03db7211328
Corwin Wright <git.hub@corwin.me.uk>
2023-08-12 16:08:26 +0100

    updated dimension-exposing functions to be more general

---
+++ Misc/expose_dim.m
@@ -0,0 +1,51 @@
+function [Matrix,DimSize,DimOrder] = expose_dim(Array,Dim)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Take an n-dimensional Matlab array and reshape such that the array
+%becomes 2D with the chosen dimension as the first and all other 
+%dimensions are merged in the second
+%
+%To restore the original dimensions assuming no changes to size, use this syntax:
+%    Restored = permute(reshape(Matrix,NewSize),NewOrder);
+%
+%If changes have been made to the exposed dimension, e.g. interpolation or selection, use this syntax:
+%    Restored = permute(reshape(Matrix,[size(Matrix,1),NewSize(2:end)]),NewOrder);
+%
+%inputs:
+%  Array: n-dimensional input array
+%  Dim:   dimension to bring to the front
+%
+%outputs:
+%  Matrix:   2D output array, as described above
+%  DimSize:  size of dimensions needed to put the array back using reshape()
+%  DimOrder: order of dimensions needed to put the array back using permute()
+%
+%Corwin Wright, c.wright@bath.ac.uk, 2023/08/12
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% do the reshaping
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%get size of original array
+sz = size(Array);
+
+%permute desired dimension to front
+DimOrder = unique([Dim,1:1:numel(sz)],'stable');
+
+%reshape to make all other dimensions lines
+Matrix = reshape(permute(Array,DimOrder),[sz(Dim),prod(sz(DimOrder(2:end)))]);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%work out the information we need to put it back
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%DimSize is used with reshape() to put all the dimensions back to the right size
+DimSize  = [size(Matrix,1),sz(DimOrder(2:end))];
+
+%NewOrder is used with permute() to put all the dimensions back in the right order
+DimOrder = 1:1:numel(sz);
+DimOrder = [DimOrder(DimOrder < Dim)+1,1,DimOrder(DimOrder > Dim)];
+
--- Misc/interp_1d_ndims.m
+++ Misc/interp_1d_ndims.m
@@ -4,6 +4,8 @@ function yi= interp_1d_ndims(x,y,xi,Dim,varargin)
 %Do 1d interpolation along a chosen dimension of an ND array
 %Corwin Wright, c.wright@bath.ac.uk, 2022/08/06
 %
+%updated 2023/08/12 to move reshaping and reordering to a separate function expose_dim() for more general use
+%
 %inputs: as interp1, except with the dimension specified as the last non-varargin input (set to [] to skip)
 %outputs: array interpolated along the chosen dimension
 %
@@ -14,25 +16,13 @@ if nargin < 4; Dim = 1; end          %assume first dimension if not specified, a
 if numel(Dim) == 0; Dim = 1; end     %if set to blank, then set first dimension
 if nargin < 5; varargin = {}; end    %make sure we have a varargin for the call to interp1 below
 
-%get size of array
-sz = size(y);
-
-%permute desired dimension to front
-DimOrder = unique([Dim,1:1:numel(sz)],'stable');
-
-%reshape to make all other dimensions lines
-y = reshape(permute(y,DimOrder),[sz(Dim),prod(sz(DimOrder(2:end)))]);
+%expose the desired dimension
+[y,a,b] = expose_dim(y,Dim);
 
 %interpolate
 yi = interp1(x,y,xi,varargin{:});
 
-%reshape back
-yi = reshape(yi,[numel(xi),sz(DimOrder(2:end))]);
-
-%and permute back
-NewOrder = 1:1:numel(sz);
-NewOrder = [NewOrder(NewOrder < Dim)+1,1,NewOrder(NewOrder > Dim)];
-yi = permute(yi,NewOrder);
-
+%put the dimensions back in order 
+yi = permute(reshape(yi,[size(yi,1),a(2:end)]),b);
 
 return
