75da56610d94787f5387f9118bd195148f92804d
Corwin Wright <git.hub@corwin.me.uk>
2023-09-22 13:36:32 +0100

    renamed GNSS to GNSS-RO

--- DatasetSpecific/get_limbsounders.m
+++ DatasetSpecific/get_limbsounders.m
@@ -56,7 +56,7 @@ InstInfo.ACE.Path           = [LocalDataDir,'/ACE/v5.2/'];
 %GNSS
 InstInfo.GNSS.TimeRange     = [datenum(2002,1,1),datenum(9999,999,999)]; %still running at time of writing
 InstInfo.GNSS.HeightRange   = [0,40];
-InstInfo.GNSS.Path          = [LocalDataDir,'/GNSS'];
+InstInfo.GNSS.Path          = [LocalDataDir,'/GNSS-RO/'];
 
 %HIRDLS
 InstInfo.HIRDLS.TimeRange   = [datenum(2005,1,29),datenum(2008,1,77)];
