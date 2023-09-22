b593628871263d67f5ab9074e17d7cb5ef3823aa
Corwin Wright <git.hub@corwin.me.uk>
2023-09-22 13:32:11 +0100

    updates to LocalDataDir to remove obsolete machines

--- System/LocalDataDir.m
+++ System/LocalDataDir.m
@@ -3,13 +3,19 @@ function LocalDataDir = LocalDataDir()
 %identifies which machine we're using, and sets the path to the data store directory accordingly
 TheComputerThisIsOn = upper(char(java.net.InetAddress.getLocalHost.getHostName));
 
+%identify the user as wll, for some systems
+TheUser = char(java.lang.System.getProperty('user.name'));
+
+
 %set data directory path
 if strcmp(TheComputerThisIsOn,'BETTERAVE'); %Corwin's 2023 XPS13
   LocalDataDir = 'C:\Data\';
 elseif strcmpi(TheComputerThisIsOn,'neils-macbook-pro')
-  LocalDataDir = '/Users/neil/Data/'
+  LocalDataDir = '/Users/neil/data/';
 elseif isunix %assume Bath Uni filesystem
-  LocalDataDir = '/u/f/cw785/Data/';
+  if     strcmp(TheUser,'cw785'); LocalDataDir = '/u/f/cw785/Data/';
+  elseif strcmp(TheUser,'nh351'); LocalDataDir = '/u/f/nh351/Data/';    
+  end
 else %assume Bath Uni filesystem
   LocalDataDir = 'Z:\Data\';
 end
