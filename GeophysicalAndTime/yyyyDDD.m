c197050503be583a2ff8888e3b6a3681a7594a05
corwin365 <git.hub@corwin.me.uk>
2023-11-25 15:13:00 +0000

    added function yyyyDDD() as almost all my programs involve generating this string!

---
+++ GeophysicalAndTime/yyyyDDD.m
@@ -0,0 +1,14 @@
+function FormattedString = yyyyDDD(DateNum)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%convert a datenum() to a char of the format YYYYdDDD,
+%where YYYY is the year and DDD is the day number within the year
+%
+%Corwin Wright, c.wright@bath.ac.uk, 2023/11/25
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[y,~,~] = datevec(DateNum);
+dn = floor(date2doy(DateNum));
+FormattedString = [sprintf('%04d',y),'d',sprintf('%03d',dn)];
+
+return
