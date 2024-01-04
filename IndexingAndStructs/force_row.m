41b08c22f8081920aa96c4e4b64f26c816e58540
corwin365 <git.hub@corwin.me.uk>
2024-01-04 22:05:55 +0000

    added option to silence warning about dimensionality

--- IndexingAndStructs/force_row.m
+++ IndexingAndStructs/force_row.m
@@ -1,17 +1,20 @@
-function Vector = force_row(Vector)
+function Vector = force_row(Vector,Silent)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Matlab function to force a 1D vector to be a row
 %
-%
+%will skip arrays with more than 1 dimension, giving a warning unless "Silent" is set to 1
 %
 %Corwin Wright, c.wright@bath.ac.uk
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%check Silent var exists
+if nargin < 2; Silent = 0; end
+
 %check we have a 1D data series
 sz = size(Vector);
 if    numel(sz) > 2 || (sz(1) ~= 1 & sz(2) ~= 1);
-  warning('foce_row() is being applied to a variable with > 1 dimension, skipping operation')
+  if Silent ~=1; warning('force_row() is being applied to a variable with > 1 dimension, skipping operation'); end
   return
 end
 
