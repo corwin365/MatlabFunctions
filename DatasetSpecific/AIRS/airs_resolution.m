07d324ef5d993871aada18906bdf39259683e40b
Corwin Wright <git.hub@corwin.me.uk>
2025-12-02 11:59:22 +0000

    updated to cope with 2D brightness functions; also removed legacy 8-entry 3D functionality, and now returns an error if this is requested

--- DatasetSpecific/AIRS/airs_resolution.m
+++ DatasetSpecific/AIRS/airs_resolution.m
@@ -1,4 +1,4 @@
-function [R,Z,N,O] = airs_resolution(Day,DoY,Lat,ZScale,v1)
+function [R,Z,N,O] = airs_resolution(Day,DoY,Lat,ZScale,Format,Channel)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %function to compute a weighted resolution profile
@@ -6,59 +6,54 @@ function [R,Z,N,O] = airs_resolution(Day,DoY,Lat,ZScale,v1)
 %linear dependence on latitude, day-of-year, and
 %whether it's day or night
 %
-%requires: 
-%     'airs3d_resolution.mat' - file containing the coefficients and baseline profiles
+%updated 2025/12/02 to:
+%  (a) provide 2D AIRS channels or 3D retrieved as options
+%  (b) remove legacy flag for original version
+%
+%requires file containing the coefficients and baseline profiles:
+%     'airs3d_resolution_e5.mat' and 'airs2d_resolution.mat'
 %
 %in:
-%  Day: 1 for daytime profile, 0 for nighttime.
-%  DoY: day of year. Can be fractional
+%  Day: 1 for daytime profile, 0 for nighttime. Ignored for 2D data.
+%  DoY: day of year. Can be fractional. Ignored for 2D case
 %  Lat: latitude
 %  ZScale: height scale to interpolate results onto, in km
-%  v1: optional - set to 1 to use the old 8-profile version, otherwise uses new ERA5-based version
+%  Format: optional - set to '2' to use 2D brightness temperature kernels,otherwise  3D used
+%  Channel: '4mu'	'15mu_high' or	'15mu_low'. Only used in 2D case
 %
 %out:
 %  R - resolution at each height
 %  Z - duplicate of requested Z scale
-%  N - noise estimate at each point
-%  O - estimated signal fraction at each point
+%  N - noise estimate at each point (only for 3D, NaN in 2D case)
+%  O - estimated signal fraction at each point (only for 3D, NaN in 2D case)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if ~exist('v1'); v1 = 0; elseif v1 ~= 1; v1 = 0; end
 
-
-if v1 == 1;
-
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %% old version
-  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-  %load data
-  load([LocalDataDir,'/AIRS/airs3d_resolution.mat'])
-
-  %stick very large values where the resoution is poorly-defined
-  Profiles(Profiles == 0) = 30;
-
-  %get resolution profile
-  [~,idx1] = min(abs(DoY-DayOfYear));
-  [~,idx2] = min(abs(Lat-Latitude));
-  if Day == 1; idx3 = 2; else idx3 = 1; end %1 is nighttime, 2 is daytime
-
-  Contribs = squeeze(RetrievalMix(idx1,idx2,idx3,:));
-
-  Profile = Profiles(:,1)*Contribs(1);
-  for iProf=2:1:8; Profile = sum([Profile,Profiles(:,iProf)*Contribs(iProf)],2,'omitnan'); end
-  Profile = Profile./sum(Contribs);
-
-  %for forwards compatability, add outputs for noise and sig frac set to NaN
-  Noise   = NaN(size(Profile));
-  ObsFrac = NaN(size(Profile));
-
-
+%handle inputs
+if ~exist('Format','var');
+  Format = 3; %i.e. 3D
+elseif Format == 1; 
+  error('Error: this is no longer a valid option for airs_resolution()') %just in case
+elseif Format ~= 2;
+  Format = 3; %i.e. 3D
 else
+  %we're in the 2D regime. What channel?
+  if nargin < 6
+    error('Error: 2D weighting functions requested but no channel requested: valid options are ''4mu'', ''15mu_high'' or	''15mu_low'' ')
+  elseif  strcmpi(Channel,      '4mu'); idx2 = 1;
+  elseif  strcmpi(Channel,'15mu_high'); idx2 = 2;
+  elseif  strcmpi(Channel, '15mu_low'); idx2 = 3;
+  else
+    error('Error: 2D weighting functions requested but invalid channel requested: valid options are ''4mu'', ''15mu_high'' or	''15mu_low'' ')
+  end
+end
+
+
+if Format == 3;
 
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-  %% new version
+  %% 3D retrieval kernels
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
   %load data
@@ -74,6 +69,39 @@ else
   Profile = squeeze(Grid.Res(   idx1,idx2,idx3,:));
   Noise   = squeeze(Grid.Noise( idx1,idx2,idx3,:));
   ObsFrac = squeeze(Grid.Signal(idx1,idx2,idx3,:));
+  
+
+elseif Format == 2;
+
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  %% 2D brightness temperature kernels
+  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+  %load data
+  Grid = load([LocalDataDir,'/AIRS/airs2d_resolution.mat']);
+
+  %work out which geographic regime we're in
+  if     abs(Lat) < 25; idx1 = find(strcmp(Grid.Region, 'Equatorial')); 
+  elseif abs(Lat) < 60; idx1 = find(strcmp(Grid.Region, 'Midlatitudes'));
+  else 
+    %polar regime. This is more complex as it depends on the time of year
+    if DoY > 80 && DoY < 265
+      if   Lat < 0; idx1 = find(strcmp(Grid.Region, 'PolarWinter'));
+      else          idx1 = find(strcmp(Grid.Region, 'PolarSummer'));  
+      end
+    else
+      if   Lat > 0; idx1 = find(strcmp(Grid.Region, 'PolarWinter'));
+      else          idx1 = find(strcmp(Grid.Region, 'PolarSummer'));  
+      end 
+    end
+  end
+
+  %extract the data
+  Z       = Grid.Z;  
+  Profile = Grid.Resolution(:,idx1,idx2);
+  Noise   = NaN(size(Z));
+  ObsFrac = NaN(size(Z));
+
   
 
 end
